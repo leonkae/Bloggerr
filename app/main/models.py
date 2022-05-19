c12dd9d26264a086147bdabe787c51b6b9e9f37b
leonkae <leon29kae@gmail.com>
2022-05-19 11:40:22 +0300

    updated models file

--- app/main/models.py
+++ app/main/models.py
@@ -35,6 +35,15 @@ class Post(db.Model):
       date_posted = db.Column(db.DateTime, nullable=False, default=datetime)
       content = db.Column(db.Text, nullable=False)
       user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-        
+      comments = db.relationship('Comments', backref='comments',lazy = True)
+
       def __repr__(self):
           return f"User('{self.title}','{self.date_posted}','{self.content}')"    
+      
+
+
+class Comments(db.Model):        
+    id = db.Column(db.Integer, primary_key = True)
+    comment = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))    
+    
