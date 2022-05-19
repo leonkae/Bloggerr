b0226ee2327f2d1fbcd51e466be7862fe793941c
leonkae <leon29kae@gmail.com>
2022-05-19 11:39:44 +0300

    updated forms file

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,2 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
+
+
+
+class PostsForm(FlaskForm):
+    title = StringField('enter title')
+    content = TextAreaField('hi Bloggerr lets hear you')
+    date_posted = StringField()
+    submit=SubmitField('Blog')
+    
+class CommentForm(FlaskForm):
+    comment = TextAreaField('comment')    
+    submit=SubmitField('submit comment')   
+    
+
+class UpdateAccount(FlaskForm):
+    bio = TextAreaField('bio')    
+    submit=SubmitField('update bio')  
