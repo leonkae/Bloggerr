5a8b52978b875166f64bea387b57a6b5191e5d51
leonkae <leon29kae@gmail.com>
2022-05-19 12:07:58 +0300

    updated views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -43,3 +43,10 @@ def login():
     
     return render_template('auth/login.html', login=login)
 
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main_blueprint.index'))
+
