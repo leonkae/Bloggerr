9dfd0ad7a200fd380300906ded54591c57b7c91e
leonkae <leon29kae@gmail.com>
2022-05-20 18:09:49 +0300

    updated flask

--- app/main/models.py
+++ app/main/models.py
@@ -1,4 +1,4 @@
-pipfrom datetime import datetime 
+from datetime import datetime 
 from werkzeug.security import generate_password_hash,check_password_hash
 from app import db
 from app import login_manager
